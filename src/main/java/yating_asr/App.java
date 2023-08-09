df9342f449bb40567af1bc5f25e3cf05f3ed5859
pinke <pinke.yu7@gmail.com>
2023-08-09 17:53:06 +0800

    Feature: initialize

---
+++ src/main/java/yating_asr/App.java
@@ -0,0 +1,31 @@
+package yating_asr;
+
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        String asrApiUrl = "ASR_ENDPOINT";
+        String asrApiKey = "PUT_YOUR_API_KEY_HERE";
+
+        String audioUri = "http://www.example.com/audio.mp3";
+        String asrModel = AsrModel.ZHEN;
+        Boolean speakerDiarization = true;
+        Integer speakerCount = 0;
+        Boolean sentiment = true;
+
+        AsrClient client = new AsrClient(asrApiUrl, asrApiKey);
+
+        try {
+            List<Sentence> sentenceList = client.Transcription(audioUri, asrModel, speakerDiarization, speakerCount,
+                    sentiment);
+
+            sentenceList.forEach((s) -> System.out.println(s.getSentence()));
+        } catch (Exception e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
+    }
+}
